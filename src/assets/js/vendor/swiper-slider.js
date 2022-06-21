c8af09ce51610afd3969bdfc2b867697e178a180
bioss7 <fireleny@gmail.com>
2022-06-21 10:50:29 +0700

    pages ui

---
+++ src/assets/js/vendor/swiper-slider.js
@@ -0,0 +1,9 @@
+console.log("Swiper slider");
+
+document.addEventListener('DOMContentLoaded', function() {
+    new Swiper('.slider-swiper', {
+        slidesPerView: 1,
+        centeredSlides: true
+    });
+});
+
